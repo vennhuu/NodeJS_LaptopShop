98a81903ed2ef14d4c323f73239b863a61ad85ed
Phan Huu Phuoc <phuochuu0512@gmail.com>
2026-05-18 15:38:15 +0700

    Hello world

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log("Hello World")
+const express = require("express") ;
+
+const app = express() ;
+
+const port = 8080 ;
+
+app.get("/" , (req , res) => {
+    res.send ("Hello World")
+})
+
+app.get("/venn" , (req , res) => {
+    res.send ("Hello Phước")
+})
+
+app.listen(8080 , () => {
+    console.log(`My app is running on port ${port}`)
+})
